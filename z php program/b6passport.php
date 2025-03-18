941fc269835232b5baff610441a049ba816720c5
Akshay Kumar A <akshayckm03@gmail.com>
2025-03-18 18:12:05 +0530

    Delete z php program directory

--- z php program/b6passport.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head></head>
-<body>
-    <h2>welcome to passport</h2>
-    create new pasport<a href="b6pass1.php">passport entry</a>
-   <br> view passport<a href="b6pass2.php"> view passport</a>
-</body>
-</html> 
